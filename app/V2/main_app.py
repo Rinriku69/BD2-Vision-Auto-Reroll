34c4ada708350d36f2b0d3e9f7444b5e181b9bc5
Rinriku69 <siripoohkim@gmail.com>
2026-03-17 23:16:09 +0700

    feat: add Type hint

--- app/V2/main_app.py
+++ app/V2/main_app.py
@@ -1,43 +1,59 @@
 # Import libraries and model
-import pyautogui # type: ignore
-import time # type: ignore
+import pyautogui 
+import time 
 import pydirectinput # type: ignore
-import keyboard # type: ignore
-import winsound # type: ignore
-import cv2 # type: ignore
-import numpy as np # type: ignore
-from ultralytics import YOLO  # type: ignore
+import keyboard
+import winsound 
+import cv2 
+from cv2.typing import MatLike
+import numpy as np 
+from ultralytics import YOLO 
+from typing import cast
+from ultralytics.engine.results import Results
 
 
 class CharacterDetection():
     
-    def __init__(self,model_path:str,characters:list[str],toggle_key:str,stop_key:str):
+    def __init__(self,char_model_path:str,ui_model_path:str,characters:list[str],toggle_key:str,stop_key:str):
         print(f"Loading Model...")
-        self.script_alive = True
-        self.script_active = False
+        # Script State
+        self.script_alive: bool = True
+        self.script_active: bool = False
+        # Load Model
         try:
-            self.model = YOLO(model_path)
+            self.char_model: YOLO = YOLO(char_model_path)
+            self.ui_model: YOLO = YOLO(ui_model_path)
             print(f"Model loaded successfully!")
         except Exception as e:
             print(f"Error Loading Model {e}")
-        self.toggle_key = toggle_key
-        self.stop_key = stop_key
+        
+        # Register hotkeys
+        self.register_hotkeys(toggle_key,stop_key)
+        
+        # Get target characters images
         base_path = 'public/characters/'
         character_image_paths = [f"{base_path}{char_file_name}.png" for char_file_name in characters]
         print('\n'.join([f"Getting Character path : {path}," for path in character_image_paths]))
-        self.chars_need = len(character_image_paths)
-        try:
-            self.template_gray = [cv2.cvtColor(cv2.imread(char_path),cv2.COLOR_BGR2GRAY) for char_path in character_image_paths]
-            self.register_hotkeys()
-        except FileNotFoundError as e:
-            print(f"ERROR: Character File path not found {e}")
-            exit()
         
-    def exit_script(self):
+        # Convert Images to BGR2GRAY
+        self.chars_need = len(character_image_paths)
+        
+        self.template_gray: list[MatLike] = []
+        for char_path in character_image_paths:
+            img = cv2.imread(char_path)
+    
+            if img is None:
+                raise ValueError(f"โหลดภาพไม่ได้: {char_path}")
+    
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            self.template_gray.append(gray)
+     
+    # Script State Control    
+    def exit_script(self) -> None :
         self.script_active = False
         self.script_alive = False
 
-    def toggle_script(self):
+    def toggle_script(self)  -> None :
         self.script_active = not self.script_active
         
         if self.script_active:
@@ -45,14 +61,14 @@ class CharacterDetection():
         else:
             print("--- Script DEACTIVATED by Hotkey ---")
             
-    def register_hotkeys(self):
+    def register_hotkeys(self,toggle_key:str,stop_key:str)  -> None :
         try:
-            keyboard.add_hotkey(self.toggle_key, self.toggle_script)
-            keyboard.add_hotkey(self.stop_key, self.exit_script)
+            keyboard.add_hotkey(toggle_key, self.toggle_script)
+            keyboard.add_hotkey(stop_key, self.exit_script)
 
             print(
-                f"Script loaded. Press {self.toggle_key.upper()} to Start/Stop. "
-                f"Press {self.stop_key.upper()} to Exit."
+                f"Script loaded. Press {toggle_key.upper()} to Start/Stop. "
+                f"Press {stop_key.upper()} to Exit."
             )
 
         except Exception as e:
@@ -60,25 +76,44 @@ class CharacterDetection():
                 f"Failed to register hotkeys. Try running the script as an administrator. Error: {e}"
             )
             exit()
+    #--------------------
     
-    def detecting_character(self):
+       
+    #  Screen detection
+    def detecting_character(self) -> list[np.ndarray] | None:
         screenshot = pyautogui.screenshot()
         frame = np.array(screenshot)
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        results = self.model(frame, conf=0.8,verbose=False)
-        found_character = []
+        results: list[Results]  = cast(list[Results],self.char_model(frame, conf=0.8,verbose=False))
+        found_character: list[np.ndarray] = []
         for r in results:
             slots = r.boxes
+            if slots == None:
+                return None    
             for slot in slots:
                 cls_id = int(slot.cls[0])
                 if cls_id == 0: 
                     x1, y1, x2, y2 = map(int, slot.xyxy[0])
-                    cropped_character = frame[y1:y2, x1:x2]
+                    cropped_character: np.ndarray = frame[y1:y2, x1:x2]
                     found_character.append(cropped_character)
         print(f"Finished Scan found {len(found_character)} 5-star characters")
         return found_character
     
-    def verify_character(self,cropped_5star_img, threshold=0.6):
+    """ def detecting_ui(self):
+        screenshot = pyautogui.screenshot()
+        frame = np.array(screenshot)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        results:list[Results]  = self.char_model(frame, conf=0.8,verbose=False)
+        for r in results:
+            buttons = r.boxes
+            #draw_again = 0, skip = 1
+            for btn in buttons:
+                cls_btn = int(btn.cls[0])
+                if cls_btn == 0:
+                    x1, y1, x2, y2 = map(int,btn.xyxy) """
+            
+    
+    def verify_character(self,cropped_5star_img: np.ndarray, threshold:float = 0.6) -> bool:
 
      
         cropped_gray = cv2.cvtColor(cropped_5star_img, cv2.COLOR_BGR2GRAY)
@@ -94,7 +129,7 @@ class CharacterDetection():
 
             result = cv2.matchTemplate(cropped_gray, template_gray, cv2.TM_CCOEFF_NORMED)
 
-            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+            _min_val, max_val, _min_loc, _max_loc = cv2.minMaxLoc(result)
 
             if max_val >= threshold:
                 print(f"Found With : {max_val*100:.2f}%")
@@ -106,10 +141,13 @@ class CharacterDetection():
     def start_reroll(self):  
         try:  
             while self.script_alive:
-                found = False
+                condition_met: bool = False
                 if self.script_active:                
-                    character_images = self.detecting_character()
-                    target_found = 0
+                    character_images:list[np.ndarray] | None = self.detecting_character()
+                    if character_images == None:
+                        return #do some thing
+                    
+                    target_found: int = 0
                     for c in character_images:
                         print("Before char check")
                         target_char_check = self.verify_character(c)
@@ -118,11 +156,12 @@ class CharacterDetection():
                             target_found += 1
                             print(f"Found {target_found} target now")
                             if target_found == self.chars_need:
-                                found = True
+                                condition_met = True
                                 break
                     self.script_active = False
                     break
-                if found:
+                # Stop Condition
+                if condition_met:
                     print("*** Target Founded ****")
                     self.script_active = False
                     print("--- Script Stopped ---")
@@ -143,7 +182,8 @@ class CharacterDetection():
                 
                 
 if __name__ == "__main__":
-    MODEL_PATH = 'model/train/runs/detect/train/weights/best.pt'
+    CHAR_MODEL_PATH = 'model/train/runs/detect/char/train/weights/best.pt'
+    UI_MODEL_PATH = 'model/train/runs/detect/ui/train2/weights/best.pt'
 
-    bot = CharacterDetection(MODEL_PATH,["rafina3","alec"],'f9','esc')
+    bot = CharacterDetection(CHAR_MODEL_PATH,UI_MODEL_PATH,["rafina3","alec"],'f9','esc')
     bot.start_reroll()
